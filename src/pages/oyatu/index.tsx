3470687b815c9c33eca53cfa6e557ee25bf8357c
akane11161218 <akane141073@gmail.com>
2024-08-12 22:18:58 +0900

    背景色付け

--- src/pages/oyatu/index.tsx
+++ src/pages/oyatu/index.tsx
@@ -1,11 +1,23 @@
+import React, { useEffect } from "react";
 import LinkButton from "../../components/LinkButton";
+import "./Oyatu.module.css";
 
 const Oyatu = () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = "#E0FFFF";
+    return () => {
+      document.body.style.backgroundColor = ""; // クリーンアップ
+    };
+  }, []);
+
   return (
     <>
-      <div>おやつ</div>
-      <LinkButton to="/">ホームへ</LinkButton>
+      <div>
+        <div>おやつ</div>
+        <LinkButton to="/">ホームへ</LinkButton>
+      </div>
     </>
   );
 };
+
 export default Oyatu;
